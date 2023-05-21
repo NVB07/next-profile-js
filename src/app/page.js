9489302cf72ac89995545ad98495c408c229e848
NVB07 <nguyenvanbinh20020712@gmail.com>
2023-05-21 22:36:00 +0700

    update fontsize

--- src/app/page.js
+++ src/app/page.js
@@ -5,12 +5,12 @@ export default function Home() {
     return (
         <div className="w-full  md:flex justify-around pt-6">
             <div className="flex-1 flex flex-col  md:items-start justify-center ">
-                <h1 className="w-full text-center md:text-start text-transparent bg-clip-text text-6xl font-bold bg-gradient-to-r from-[#7927b2] to-[#fb3182]">
+                <h1 className="w-full text-center md:text-start text-transparent bg-clip-text text-5xl md:text-6xl font-bold bg-gradient-to-r from-[#7927b2] to-[#fb3182]">
                     Hi. I&apos;m NVB
                 </h1>
                 <div className=" whitespace-nowrap relative text-4xl ">
                     <p className=" text-4xl w-full text-center md:text-start pb-1 font-semibold">Front-End </p>
-                    <div className="text-3xl md:absolute flex justify-center relative">
+                    <div className="text-2xl min-[400px]:text-[28px] md:text-3xl md:absolute flex justify-center relative">
                         <TextSlider />
                     </div>
                 </div>
