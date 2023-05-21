7be18d0df70192b862f7a9a51975f72049cb00e0
NVB07 <nguyenvanbinh20020712@gmail.com>
2023-05-21 22:22:16 +0700

    upsate

--- src/app/page.js
+++ src/app/page.js
@@ -6,11 +6,11 @@ export default function Home() {
         <div className="w-full  md:flex justify-around pt-6">
             <div className="flex-1 flex flex-col  md:items-start justify-center ">
                 <h1 className="w-full text-center md:text-start text-transparent bg-clip-text text-6xl font-bold bg-gradient-to-r from-[#7927b2] to-[#fb3182]">
-                    Hi. I'm NVB
+                    Hi. I&apos;m NVB
                 </h1>
                 <div className=" whitespace-nowrap relative text-4xl ">
                     <p className=" text-4xl w-full text-center md:text-start pb-1 font-semibold">Front-End </p>
-                    <div className="text-3xl md:absolute relative">
+                    <div className="text-3xl md:absolute flex justify-center relative">
                         <TextSlider />
                     </div>
                 </div>
